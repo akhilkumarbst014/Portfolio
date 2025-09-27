4c584a97eb2df752ef015ccee85a1ddf200f7c24
akhilkumarbst014 <akhileshkumarbst014>
2025-09-27 10:36:43 +0530

    updation-in-portfolio

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -851,7 +851,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
     };
 
     try {
-        const response = await fetch('https://akhilesh-kumar-portfolio.onrender.com/send-email', { // update URL if needed
+        const response = await fetch('https://akhilesh-kumar-portfolio.onrender.com/send-email', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
